38c3478e7034394f6ab48d0d36af5ae7a843adef
MDARQUM734 <arqum734@gmail.com>
2025-10-31 18:28:09 +0530

    Time: 228 ms (46.82%) | Memory: 43 MB (5.11%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean isSafe(int row, int col,char[][] board,char num){
+        for(int i=0; i<9; i++){
+            if(board[row][i] == num) return false;
+        }
+        for(int i=0; i<9; i++){
+            if(board[i][col] == num) return false;
+        }
+        int sRow = row/3*3;
+        int sCol = col/3*3;
+        for(int i=sRow; i<sRow+3; i++){
+            for(int j=sCol; j<sCol+3; j++){
+                if(board[i][j] == num) return false;
+            }
+        }
+        return true;
+    }
+    public void solve(char[][] board,int row, int col,char[][] grid){
+        if(row == 9){
+            for(int i=0; i<9; i++){
+                for(int j=0; j<9; j++){
+                    grid[i][j] = board[i][j];
+                }
+            }
+            return;
+        }
+        else if(board[row][col] != '.'){
+            if(col != 8) solve(board,row,col+1,grid);
+            else solve(board,row+1,0,grid);
+        }
+        else{
+            for(char ch ='1'; ch<='9'; ch++){
+                if(isSafe(row,col,board,ch)){
+                    board[row][col] = ch;
+                    if(col != 8) solve(board,row,col+1,grid);
+                    else solve(board,row+1,0,grid);
+                    board[row][col] = '.';
+                }
+            }
+        }
+    }
+    public void solveSudoku(char[][] board) {
+        char[][] grid = new char[9][9];
+        solve(board,0,0,grid);
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                board[i][j] = grid[i][j];
+            }
+        }
+    }
+}
+
+
