530f1874891e61c49021d8af0ee2ef5e1b7e0765
MDARQUM734 <arqum734@gmail.com>
2025-11-04 22:01:56 +0530

    Time: 10 ms (5.18%) | Memory: 46.4 MB (5.47%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,18 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(candidates,target,0,new ArrayList<>(),result);
+        Set<List<Integer>> uniqueResults = new HashSet<>();
+        backtrack(candidates,target,0,new ArrayList<>(),result,uniqueResults);
         return result;
     }
-    public void backtrack(int[] candidates, int target,int idx, List<Integer> list,List<List<Integer>> result){
+    public void backtrack(int[] candidates, int target,int idx, List<Integer> list,List<List<Integer>> result,Set<List<Integer>> uniqueResults){
         if(target == 0){
-            result.add(new ArrayList<>(list));
+            if(!uniqueResults.contains(list)){
+                uniqueResults.add(new ArrayList<>(list));
+                result.add(new ArrayList<>(list));
+            }
             return;
         }
         if(target < 0 || idx == candidates.length) return;
         list.add(candidates[idx]);
-        backtrack(candidates,target-candidates[idx],idx,list,result);
+        backtrack(candidates,target-candidates[idx],idx+1,list,result,uniqueResults); // for taking one value once 
+        backtrack(candidates,target-candidates[idx],idx,list,result,uniqueResults); // for taking same value again and again
         list.remove(list.size()-1);
-        backtrack(candidates,target,idx+1,list,result);
+        backtrack(candidates,target,idx+1,list,result,uniqueResults);
     }
 }
