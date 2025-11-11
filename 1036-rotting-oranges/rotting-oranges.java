8f2716a31e499d2e5a792f14f16e13d5663b6028
MDARQUM734 <arqum734@gmail.com>
2025-11-11 18:59:16 +0530

    Time: 1 ms (100.00%) | Memory: 43.6 MB (18.07%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,38 +2,34 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        Queue<int[]> pq = new LinkedList<>();
-        int fresh = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j] == 2){
-                    pq.add(new int[]{i,j});
-                }else if(grid[i][j] == 1){
-                    fresh++;
-                }
-            }
-        }
-        if(fresh == 0) return 0;
-        int dir[][] ={{-1,0},{1,0},{0,1},{0,-1}};
-        int minutes = -1;
-        while(!pq.isEmpty()){
-            int size = pq.size();
-            minutes++;
-            while(size > 0){
-                int curr[] = pq.remove();
-                int x = curr[0], y= curr[1];
-                for(int d[] : dir){
-                    int nx = x + d[0];
-                    int ny = y + d[1];
-                    if(nx >= 0 && nx < n && ny>=0 && ny < m && grid[nx][ny]==1){
-                        fresh--;
-                        grid[nx][ny] = 2;
-                        pq.add(new int[]{nx,ny});
+        int num = 2;
+        boolean changed = false;
+        while(true){
+            changed = false;
+            for(int i=0; i<n; i++){
+                for(int j=0; j<m; j++){
+                    if(grid[i][j] == num){
+                        if (i > 0 && grid[i-1][j] == 1) { 
+                            grid[i-1][j] = num + 1; 
+                            changed = true; }
+                        if (i < n-1 && grid[i+1][j] == 1) { 
+                            grid[i+1][j] = num + 1; 
+                            changed = true; }
+                        if (j > 0 && grid[i][j-1] == 1) { 
+                            grid[i][j-1] = num + 1; 
+                            changed = true; }
+                        if (j < m-1 && grid[i][j+1] == 1) { 
+                            grid[i][j+1] = num + 1; 
+                            changed = true; }
                     }
                 }
-                size--;
             }
+            if(!changed) break;
+            else num++;
         }
-        return (fresh == 0) ? minutes : -1;
+        for (int[] row : grid)
+        for (int cell : row)
+            if (cell == 1) return -1;
+        return num-2;
     }
 }
