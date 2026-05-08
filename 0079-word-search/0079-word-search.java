3e79ccb88fc605019a6d5183ff3862f57c51d826
MDARQUM734 <arqum734@gmail.com>
2026-05-08 23:50:29 +0530

    Time: 133 ms (61.5%), Space: 43.2 MB (20.24%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean helper(char[][] board, String word,int i,int j,int k){
+        if(k == word.length()){
+            return true;
+        }
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j] != word.charAt(k)){
+            return false;
+        }
+        char temp = board[i][j];
+        board[i][j] = '*';
+        boolean temp2 = helper(board,word,i+1,j,k+1) || helper(board,word,i-1,j,k+1) || helper(board,word,i,j+1,k+1) || helper(board,word,i,j-1,k+1);
+        board[i][j] = temp;
+        return temp2;
+    }
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(helper(board,word,i,j,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
