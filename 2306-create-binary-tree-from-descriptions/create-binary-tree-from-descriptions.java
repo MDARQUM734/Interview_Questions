fcb32d4f1f30eadcd4e8ed14e8016984c14438a4
MDARQUM734 <arqum734@gmail.com>
2026-06-07 14:37:55 +0530

    Time: 80 ms (12.90%) | Memory: 86.1 MB (54.34%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
@@ -1,32 +1,30 @@
 class Solution {
     public TreeNode createBinaryTree(int[][] descriptions) {
         int n = descriptions.length;
-        int m = descriptions[0].length;
         HashMap<Integer,TreeNode> map = new HashMap<>();
         HashSet<Integer> set = new HashSet<>();
         for(int d[] : descriptions){
-            int parentVal = d[0];
-            int childVal = d[1];
-            int isChild = d[2];
-
-            map.putIfAbsent(parentVal,new TreeNode(parentVal));
-            map.putIfAbsent(childVal, new TreeNode(childVal));
-            TreeNode parent = map.get(parentVal);
-            TreeNode child = map.get(childVal);
-            if(isChild == 1){
-                parent.left = child;
-            }else{
-                parent.right = child;
+            int par = d[0];
+            int child = d[1];
+            int isLeft = d[2];
+            map.putIfAbsent(par,new TreeNode(par));
+            map.putIfAbsent(child,new TreeNode(child));
+            TreeNode parentNode = map.get(par);
+            TreeNode childNode = map.get(child);
+            if(isLeft == 1){
+                parentNode.left = childNode;
             }
-            set.add(childVal);
+            else{
+                parentNode.right = childNode;
+            }
+            set.add(child);
         }
-        TreeNode root = null;
-        for(int val : map.keySet()){
-            if(!set.contains(val)){
-                root = map.get(val);
-                break;
+        for(int d[]:descriptions){
+            int num = d[0];
+            if(!set.contains(num)){
+                return map.get(num);
             }
         }
-        return root;
+        return null;
     }
 }
